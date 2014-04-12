807b1c7b9824851789e2a40115a0bbee2f445503
Appricott <Appricott@users.noreply.github.com>
2014-04-12 19:23:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        
+        <title>Price Tracker</title>
+        
+             <link rel="stylesheet" href="stylesheet.css" />
+        
+    </head>
+    
+    <body>
+
+        <header>
+            <h1>Price Tracker</h1>
+            <h2><a href="#">Evaluarea produselor in functie de pret</a></h2>
+        </header>
+        
+        <nav>
+            <ul class="NavBar">
+                <li><a href="#" class="homeIcon">Acasa</a></li>
+                <li><a href="#">Noutati</a></li>
+                <li><a href="#">Despre PriceTracker</a></li>
+                <li><a href="#">Servicii</a></li>
+                <li><a href="#">Alatura-te PriceTracker</a></li>
+            </ul>
+        </nav>
+        
+        <footer><img src="img/Appricot.png"></footer>
+            
+    </body>
+</html>
