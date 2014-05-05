a4865ef3762095b7e820aefc0a126f3d95ea2937
Appricott <Appricott@users.noreply.github.com>
2014-05-05 15:40:10 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,22 +1,49 @@
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<html>
+    <head>
+        <meta charset="utf-8" />
+        
+        <title img src="img/Appricot.png">Price Tracker</title>
+        
+             <link rel="stylesheet" href="stylesheet.css" />
+        
+    </head>
+    
+    <body>
+
+        <header>
+            <h1>Price Tracker</h1>
+            <h2><a href="index.php">Evaluarea produselor in functie de pret</a></h2>
+        </header>
+        
+        <nav>
+            <ul class="NavBar">
+                <li><a href="index.php" class="homeIcon">Acasa</a></li>
+                <li><a href="#">Noutati</a></li>
+                <li><a href="#">Despre PriceTracker</a></li>
+                <li><a href="#">Servicii</a></li>
+                <li><a href="login.php">Alatura-te PriceTracker</a></li>
+            </ul>
+        </nav>
+        
+		<table width="300" border="0" align="center" cellpadding="0" cellspacing="1">
 		<tr>
 		
 			<form name="form1" method="post" action="verifica.php">
 
 				<td>
-					<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+					<table width="100%" border="0" cellpadding="3" cellspacing="1">
 						<tr>
 							<td colspan="3" align = "center"><strong>Autentificare </strong></td>
 						</tr>
 						<tr>
 							<td width="78">Username</td>
 							<td width="6">:</td>
-							<td width="294"><input name="myusername" type="text" id="myusername"></td>
+							<td width="294"><input name="username" type="text" id="username"></td>
 						</tr>
 						<tr>
 							<td>Parola</td>
 							<td>:</td>
-							<td><input name="mypassword" type="text" id="mypassword"></td>
+							<td><input name="password" type="text" id="password"></td>
 						</tr>
 						<tr>
 							<td>&nbsp</td>
@@ -28,3 +55,8 @@
 			</form>
 		</tr>
 	</table>
+		
+        <footer><img src="img/Appricot.png"></footer>
+            
+    </body>
+</html>
